0466a5bf200b56068341e5d07f060f51238ad628
Aaron <1213598592@qq.com>
2022-06-20 13:54:56 +0800

    dim层

--- gmall-realtime-2022/gmall-realtime/src/main/java/com/iflytek/common/GmallConfig.java
+++ gmall-realtime-2022/gmall-realtime/src/main/java/com/iflytek/common/GmallConfig.java
@@ -6,7 +6,6 @@ package com.iflytek.common;
  */
 
 public class GmallConfig {
-
     // Phoenix库名
     public static final String HBASE_SCHEMA = "GMALL_REALTIME";
 
@@ -15,11 +14,4 @@ public class GmallConfig {
 
     // Phoenix连接参数
     public static final String PHOENIX_SERVER = "jdbc:phoenix:hadoop101,hadoop102,hadoop103:2181";
-
-    // ClickHouse 驱动
-    // public static final String CLICKHOUSE_DRIVER = "ru.yandex.clickhouse.ClickHouseDriver";
-
-    // ClickHouse 连接 URL
-    // public static final String CLICKHOUSE_URL = "jdbc:clickhouse://hadoop101:8123/gmall";
-
 }
--- gmall-realtime-2022/gmall-realtime/src/main/java/com/iflytek/utils/MyKafkaUtil.java
+++ gmall-realtime-2022/gmall-realtime/src/main/java/com/iflytek/utils/MyKafkaUtil.java
@@ -4,26 +4,27 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.util.Properties;
 
+/**
+ * @author Aaron
+ * @date 2022/6/18 23:00
+ */
+
 public class MyKafkaUtil {
-
     private static Properties properties = new Properties();
-    private static final String BOOTSTRAP_SERVERS = "hadoop101:9092";
-
-    static {
-        properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-    }
+    private static final String BOOTSTRAP_SERVERS = "192.168.10.101:9092";
 
     public static FlinkKafkaConsumer<String> getKafkaConsumer(String topic, String group_id) {
 
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, group_id);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        // properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        // properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
         return new FlinkKafkaConsumer<String>(topic,
                 new KafkaDeserializationSchema<String>() {
@@ -48,56 +49,4 @@ public class MyKafkaUtil {
                 },
                 properties);
     }
-
-    public static FlinkKafkaProducer<String> getKafkaProducer(String topic) {
-        return new FlinkKafkaProducer<String>(topic,
-                new SimpleStringSchema(),
-                properties);
-    }
-
-    /**
-     * Kafka-Source DDL 语句
-     *
-     * @param topic   数据源主题
-     * @param groupId 消费者组
-     * @return 拼接好的 Kafka 数据源 DDL 语句
-     */
-    public static String getKafkaDDL(String topic, String groupId) {
-        return " with ('connector' = 'kafka', " +
-                " 'topic' = '" + topic + "'," +
-                " 'properties.bootstrap.servers' = '" + BOOTSTRAP_SERVERS + "', " +
-                " 'properties.group.id' = '" + groupId + "', " +
-                " 'format' = 'json', " +
-                " 'scan.startup.mode' = 'latest-offset')";
-    }
-
-    /**
-     * Kafka-Sink DDL 语句
-     *
-     * @param topic 输出到 Kafka 的目标主题
-     * @return 拼接好的 Kafka-Sink DDL 语句
-     */
-    public static String getUpsertKafkaDDL(String topic) {
-
-        return "WITH ( " +
-                "  'connector' = 'upsert-kafka', " +
-                "  'topic' = '" + topic + "', " +
-                "  'properties.bootstrap.servers' = '" + BOOTSTRAP_SERVERS + "', " +
-                "  'key.format' = 'json', " +
-                "  'value.format' = 'json' " +
-                ")";
-    }
-
-    public static String getTopicDbDDL(String groupId) {
-        return "CREATE TABLE topic_db ( " +
-                "  `database` String, " +
-                "  `table` String, " +
-                "  `type` String, " +
-                "  `data` Map<String,String>, " +
-                "  `old` Map<String,String>, " +
-                "  `pt` AS PROCTIME() " +
-                ")" + MyKafkaUtil.getKafkaDDL("topic_db", groupId);
-    }
-
-
 }
